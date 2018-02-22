0c36adac396f6d9bbee2b725b7222b49f0861a57
janus_wel <janus.wel.3@gmail.com>
2018-02-22 17:35:13 +0900

    :wrench::memo: add command to build wasm

---
+++ README.md
@@ -0,0 +1,6 @@
+Todo
+===
+
+```sh
+rustc +nightly --target wasm32-unknown-unknown -O src/lib.rs --crate-type=cdylib
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod todo {
+    #[no_mangle]
     pub struct Todo {
         name: String,
         is_done: bool,
     }
 
     impl Todo {
+        #[no_mangle]
         pub fn new(name_: &str) -> Todo {
             Todo {
                 name: name_.to_string(),
@@ -12,15 +14,19 @@ pub mod todo {
             }
         }
 
+        #[no_mangle]
         pub fn get_name(&self) -> &String {
             &self.name
         }
+        #[no_mangle]
         pub fn is_done(&self) -> bool {
             self.is_done
         }
+        #[no_mangle]
         pub fn done(& mut self) {
             self.is_done = true
         }
+        #[no_mangle]
         pub fn undone(& mut self) {
             self.is_done = false
         }
